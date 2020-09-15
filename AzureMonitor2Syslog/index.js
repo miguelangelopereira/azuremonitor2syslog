f45f3b4b52cab9dd4ce1503f9d850c993ac8c3c7
Miguel Pereira <33688013+miguelangelopereira@users.noreply.github.com>
2020-09-15 13:26:35 +0100

    Update index.js
    added facilities configuration and other validations for better troubleshooting

--- AzureMonitor2Syslog/index.js
+++ AzureMonitor2Syslog/index.js
@@ -4,8 +4,14 @@
 // miguelp@microsoft.com
 
 module.exports = function (context, myEventHubMessage) {
+    context.log(`JavaScript eventhub trigger function called for message array ${myEventHubMessage}`);
     // initializing syslog
-    var syslog = require("syslog-client");
+    try{
+        var syslog = require("syslog-client");
+    } catch (e) {
+        throw e;
+    }    
+    
 
     // getting environment variables
     var SYSLOG_SERVER = GetEnvironmentVariable("SYSLOG_SERVER");
@@ -17,7 +23,7 @@ module.exports = function (context, myEventHubMessage) {
     }
 
     var SYSLOG_HOSTNAME;
-    if (GetEnvironmentVariable("SYSLOG_HOSTNAME")=="") {
+    if (GetEnvironmentVariable("SYSLOG_HOSTNAME")==null) {
         SYSLOG_HOSTNAME = "azurefunction"
     } else {
         SYSLOG_HOSTNAME = GetEnvironmentVariable("SYSLOG_HOSTNAME");
@@ -25,11 +31,19 @@ module.exports = function (context, myEventHubMessage) {
 
     var SYSLOG_PORT = GetEnvironmentVariable("SYSLOG_PORT");
 
+    var SYSLOG_FACILITY;
+    if (GetEnvironmentVariable("SYSLOG_FACILITY")==null) {
+        SYSLOG_FACILITY = syslog.Facility.Local0;
+    } else {
+        SYSLOG_FACILITY = GetEnvironmentVariable("SYSLOG_FACILITY");
+    }
+
     // options for syslog connection
     var options = {
         syslogHostname: SYSLOG_HOSTNAME,
         transport: SYSLOG_PROTOCOL,    
-        port: SYSLOG_PORT
+        port: SYSLOG_PORT,
+        facility: SYSLOG_FACILITY
     };
 
     // log connection variables
@@ -37,9 +51,14 @@ module.exports = function (context, myEventHubMessage) {
     context.log('SYSLOG Port: ', SYSLOG_PORT);
     context.log('SYSLOG Protocol: ', SYSLOG_PROTOCOL);
     context.log('SYSLOG Hostname: ', SYSLOG_HOSTNAME);
+    context.log('SYSLOG Facility: ', SYSLOG_FACILITY);
+
+    if ((SYSLOG_SERVER == null) || (SYSLOG_PORT == null) || (SYSLOG_PROTOCOL == null) || (SYSLOG_HOSTNAME == null)) {
+        throw "Please setup environment variables. "
+    }
 
     // log received message from event hub
-    context.log('Event Hubs trigger function processed message: ', myEventHubMessage);
+    context.log(`Event Hubs trigger function processed message:  ${myEventHubMessage}`);
     context.log('EnqueuedTimeUtc =', context.bindingData.enqueuedTimeUtc);
     context.log('SequenceNumber =', context.bindingData.sequenceNumber);
     context.log('Offset =', context.bindingData.offset);
